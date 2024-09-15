42a2b59f80f50247e00cec48f354fbabe04f2f62
PierreSameh <captainpirzon@gmail.com>
2024-09-15 14:44:47 +0300

    #P push

--- app/Http/Controllers/User/CategoriesController.php
+++ app/Http/Controllers/User/CategoriesController.php
@@ -27,7 +27,7 @@ class CategoriesController extends Controller
 
     public function search(Request $request) {
         $search = $request->search ? $request->search : '';
-        $categories = Category::latest()->where('name', 'like', '%' . $search . '%')
+        $categories = Category::where('name', 'like', '%' . $search . '%')
         ->orWhere('description', 'like', '%' . $search . '%')->orderBy('position', 'asc')->get();
 
         return $this->handleResponse(
